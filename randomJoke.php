0c97bad6365e1667496db267ad4eb7724460c779
Vladyslav Mostovych <m0stwan1ed@gmail.com>
2016-12-16 08:49:46 +0100

    First project commit

---
+++ randomJoke.php
@@ -0,0 +1,7 @@
+<?php
+	echo 123;
+	$url = 'http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list';
+	echo $url;
+	echo file_get_contents('http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list');
+	//echo $json;
+?>
