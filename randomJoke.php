6c3099d8508b2adfc4d8a389ea4ad418fe579e71
Vladyslav Mostovych <m0stwan1ed@gmail.com>
2016-12-16 09:28:57 +0100

    Last commit w/o Symfony and oop

--- randomJoke.php
+++ randomJoke.php
@@ -1,7 +1,15 @@
 <?php
-	echo 123;
-	$url = 'http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list';
-	echo $url;
-	echo file_get_contents('http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list');
-	//echo $json;
+	$json = file_get_contents('http://ec2-35-156-131-145.eu-central-1.compute.amazonaws.com/list');//get json
+	$objectJson = json_decode($json);//decode json
+	
+	$pos = rand(0, 9);//random 0-9
+	
+	//output
+	echo $objectJson[$pos] -> author;
+	echo '<br>';
+	echo $objectJson[$pos] -> content;
+	echo '<br>';
+	echo $objectJson[$pos] -> id;
+	echo '<br>';
+	echo $objectJson[$pos] -> year;
 ?>
